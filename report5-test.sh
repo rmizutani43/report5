a5e64ad5c4381c348733bc0752b02a0c8026098d
A Virtual <b2131rm@aiit.ac.jp>
2021-08-01 20:13:05 +0900

    preparing files

--- report5-test.sh
+++ report5-test.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-./report5.sh
-input1=$str1
-input2=$str2
+#test1:input_check
+#test2:input_value=not0
+#test3:input_not_charactor
 
-if [ $# -ne 2] 
-then
-  echo Error argument count
-  exit 1;
+#test1
+echo "ERROR: PLZ INPUT 2 ARGUMENTS" > /tmp/$$-ans
+./report5.sh $# = 0 > /tmp/$$-result || exit 1
+
+#test1-2
+if [ $# = 0 ]; then
+    echo"INPUT0_error"
+    exit 1
+else
+    echo"OK"
 fi
 
+if [ $# != 2 ]; then
+    echo INPUT1_error: $*
+    exit 1
+else
+    echo"OK"
+fi
+#test2
+echo "ERROR: PLZ INPUT VALUE not 0"
+./report5.sh 0 a > /tmp/$$-result || exit 1
 
+#after
+rm /tmp/$$-*
