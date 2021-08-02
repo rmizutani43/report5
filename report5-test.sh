4a0e810985f131b5f7fd93c186a19064caf14a8b
A Virtual <b2131rm@aiit.ac.jp>
2021-08-03 02:30:21 +0900

    preparing files

--- report5-test.sh
+++ report5-test.sh
@@ -3,10 +3,31 @@
 #test1:input_check
 #test3:input_not_charactor
 
-#error
-echo "ERROR: PLZ INPUT 2 ARGMENTS" > /tmp/$$-ans
-./report5.sh $# -lt 0  > /tmp/$$-result && exit 1
-diff /tmp/$$-ans /tmp/$$-result || exit 1
 
-#delete
-rm /tmp/$$-*
+#パラメータ(引数)が空かチェックする
+./report5.sh $1 $2 
+
+a=$1
+b=$2
+
+if [[ $# -ne 2 ]]; then
+    echo "err 2 arg"
+    exit 1
+else
+    :
+fi
+
+if [[ $1 =~ ^[0-9]+$ ]]; then
+    :
+else
+    echo "err:par1 or 2 str"
+    exit 1
+fi
+
+if [[ $2 =~ ^[0-9]+$ ]]; then
+    :
+else
+    echo "err:par2 str"
+    exit 1
+fi
+
--- report5.sh
+++ report5.sh
@@ -3,8 +3,25 @@
 a=$1
 b=$2
 
-if [ $# -ne 2 ]; then
+if [[ $# -ne 2 ]]; then
     echo "ERROR: PLZ INPUT 2 ARGMENTS"
+    exit 1
+else
+    :
+fi
+
+if [[ $1 =~ ^[0-9]+$ ]]; then
+    :
+else
+    echo "ERROR: Parameter1 or 2 Strings"
+    exit 1
+fi
+
+
+if [[ $2 =~ ^[0-9]+$ ]]; then
+	    :
+else
+    echo "ERROR: Parameter2 Strings"
     exit 1
 fi
 
@@ -20,17 +37,19 @@ while [ 0 -lt $b ]
 
 if [ $buf1 -eq 0 ]
  then
-  echo "err1"
+  echo "Parameter1 can't 0"
   exit 1
 elif [ $buf2 -eq 0 ]
 then
-  echo "err2"
+  echo "Parameter2 can't 0"
   exit 1
 elif [ $a -eq $buf2 ]
 then
-  echo "$buf2"
+  fb=$buf2
+  echo "gcd = $fb"
   exit 0
 else
-  echo "$a"
+  fa=$a
+  echo "gcd = $fa"
   exit 0
 fi
